f5689a11de5e2cde095110d594f05039849aacb8
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-22 18:50:46 +0330

    audio.play()

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const pianokeys=document.querySelectorAll('.piano-keys .key');
 pianokeys.forEach(function(key){
     key.addEventListener('click',function(){
-        console.log('key'+key.dataset.key+'clicked');
-        
+      const keySound=key.dataset.key;
+     const audio=new Audio(`notes/${keySound}.mp3`);
+        audio.play();
     })
 })
