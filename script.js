e32c1925e0592bd00d16e6b579d164c20f6f41af
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-22 18:55:56 +0330

    css active

--- script.js
+++ script.js
@@ -4,5 +4,10 @@ pianokeys.forEach(function(key){
       const keySound=key.dataset.key;
      const audio=new Audio(`notes/${keySound}.mp3`);
         audio.play();
+
+        key.classList.add('active');
+        setTimeout(function(){
+            key.classList.remove('active');
+        },200)
     })
 })
